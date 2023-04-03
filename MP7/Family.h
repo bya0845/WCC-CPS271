c7cfdcafd79f2bd16238fabbf5d004f7fafbd438
bya0845 <byang0845@gmail.com>
2023-04-02 23:40:03 -0400

    1

--- MP7/Family.h
+++ MP7/Family.h
@@ -1,1 +1,37 @@
-#pragma once
+#ifndef FAMILY_H
+#define FAMILY_H
+
+#include <string>
+#include <vector>
+
+#include "Person.h"
+
+using std::string;
+
+class Family
+{
+private:
+    std::vector<Person> f;
+
+public:
+    Family();
+
+    /*"add" a family member by passing in Person object and storing it in the array */
+    void addFamilyMember(Person p);
+
+    // get family member at i-th array position
+    Person getFamilyMember(int i) const;
+
+    // return the number of family members
+    int numFamilyMembers() const;
+
+    // check if a person with the given firstname is in Family
+    bool findPerson(string firstName) const;
+    // return a list of all family members who are parents ("Fathe or Mother")
+    string getParents() const;
+
+    // return a list of all family members who are kids
+    string getChildren() const;
+};
+
+#endif
--- MP7/Person.h
+++ MP7/Person.h
@@ -1,1 +1,29 @@
-#pragma once
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using std::string;
+
+class Person
+{
+private:
+    string _firstname;
+    string _lastname;
+    int _age;
+    string _role;      // "Father", "Mother", "Child", "Grandmother", etc.
+
+public:
+    Person();
+    Person(string firstname, string lastname, int age, string role);
+
+    string getFirstName() const;
+    string getLastName() const;
+    string getFullName() const;
+    string getRole() const;
+    int getAge() const;
+
+    void setAge(int age);
+};
+
+
+#endif
